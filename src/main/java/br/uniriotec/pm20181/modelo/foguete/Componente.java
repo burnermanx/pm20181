0760dfc8d0e212f148c70eff116e24e846a9881d
Thiago Passos <thiago.passos@mobicare.com.br>
2018-06-20 23:06:15 -0300

    Complementando comentários e utilizando apenas os getters e setters para acessar os atributos

--- src/main/java/br/uniriotec/pm20181/modelo/foguete/Componente.java
+++ src/main/java/br/uniriotec/pm20181/modelo/foguete/Componente.java
@@ -14,62 +14,57 @@ import java.util.*;
 /**
  * Classe abstrata referente a um componente que constitui um foguete
  */
-
 public abstract class Componente {
     @Getter @Setter private String tipo;
     @Getter @Setter private String nome;
-    @Getter @Setter protected Double peso;
+    @Getter @Setter private Double peso;
     @Getter @Setter private Double diametro;
-    @Getter @Setter protected Boolean ativado;
-    protected Map<Direcao, Componente> componentes = new HashMap<>();
+    @Getter @Setter private Boolean ativado;
+    @Getter private final Map<Direcao, Componente> componentes = new HashMap<>();
 
 
     /**
      * O cosntrutor rece o tipo do componente
-     * @param tipo
+     * @param tipo tipo do componente
      */
-    public Componente(String tipo) {
-        this.tipo = tipo;
+    Componente(String tipo) {
+        setTipo(tipo);
     }
 
 
     /**
      * Anexa um componente ao próprio na direção especificada
-     * @param direcao
-     * @param componente
+     * @param direcao direção onde o componente será encaixado
+     * @param componente objeto Componente a ser encaixado
      */
     void adicionaComponenteConectado(Direcao direcao, Componente componente) {
-        componentes.put(direcao, componente);
+        getComponentes().put(direcao, componente);
     }
 
 
     /**
      * Busca um componente dada a sua direção
-     * @param direcao
-     *  @return retorna um objeto Componente ou nulo se o componente não for encontrado
+     * @param direcao direcao de onde o Componente tem de ser pego
+     * @return retorna um objeto Componente ou nulo se o componente não for encontrado
      */
     Componente pegaComponenteConectado(Direcao direcao) {
-        return componentes.get(direcao);
+        return getComponentes().get(direcao);
     }
 
 
     /**
      * Busca um componente dado seu nome
-     * @param nome
+     * @param nome nome do componente
      * @return  retorna um objeto Componente ou nulo se o componente não for encontrado
      */
     Componente pegaComponenteNome(@NonNull String nome) {
         //Se o nome do componente sendo pesquisado já for do próprio, retorne
-        if (nome.contentEquals(this.nome)) {
+        if (nome.contentEquals(this.getNome())) {
             return this;
         }
 
-
-        //Criando lista com todos os objetos de componente anexados a sua volta
-        List<Componente> componentesDisponiveis = new ArrayList<>(componentes.values());
-
-        for (Componente componente : componentesDisponiveis) {
-            Componente componenteEncontrado = null;
+        for (Componente componente : getComponentes().values()) {
+            Componente componenteEncontrado;
             componenteEncontrado = componente.pegaComponenteNome(nome);
             if (componenteEncontrado != null) {
                 return componenteEncontrado;
@@ -81,17 +76,14 @@ public abstract class Componente {
 
 
     Double getMassa() {
-        double pesoTotal = this.peso;
+        double pesoTotal = this.getPeso();
 
         //Se não tem componentes anexados ao componente, retorne o peso do próprio
-        if (componentes.isEmpty()) {
+        if (getComponentes().isEmpty()) {
             return pesoTotal;
         }
-        //Criando lista de componentescom todos os objetos de componentes conectados
-        List<Componente> componentesConectados = new ArrayList<>();
-        componentes.forEach((direcao, componente) -> componentesConectados.add(componente));
 
-        for (Componente componente : componentesConectados) {
+        for (Componente componente : getComponentes().values()) {
             pesoTotal = pesoTotal + componente.getMassa();
         }
 
@@ -103,26 +95,26 @@ public abstract class Componente {
      * @return a maior área possível do foguete visto por cima
      */
     Double getArea() {
-        double areaRedor = AreaUtil.areaCirculo(this.diametro);
+        double areaRedor = AreaUtil.areaCirculo(this.getDiametro());
 
         //Se não tenho componentes ao redor do componente, retorne a área do próprio.
-        if (componentes.isEmpty()) {
+        if (getComponentes().isEmpty()) {
             return areaRedor;
         }
 
         double areaAbaixo = 0.0;
         List<Componente> componentesAoRedor = new ArrayList<>();
-        componentes.forEach(((direcao, componente) -> {
+        getComponentes().forEach(((direcao, componente) -> {
             if (direcao != Direcao.ABAIXO) {
                 componentesAoRedor.add(componente);
             }
         }));
 
         for (Componente componente : componentesAoRedor) {
-            areaRedor = areaRedor + AreaUtil.areaCirculo(componente.diametro);
+            areaRedor = areaRedor + AreaUtil.areaCirculo(componente.getDiametro());
         }
 
-        Componente componenteAbaixo = componentes.get(Direcao.ABAIXO);
+        Componente componenteAbaixo = getComponentes().get(Direcao.ABAIXO);
         if (componenteAbaixo != null) {
             areaAbaixo = componenteAbaixo.getArea();
         }
