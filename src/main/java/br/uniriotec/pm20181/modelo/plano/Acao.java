bae15029cc40cb9d0705d0329182cd13252034c4
Thiago Passos <thiago.passos@mobicare.com.br>
2018-06-20 23:15:31 -0300

    Mudando ação para enumeração e comentando classes

--- src/main/java/br/uniriotec/pm20181/modelo/plano/Acao.java
+++ src/main/java/br/uniriotec/pm20181/modelo/plano/Acao.java
@@ -4,15 +4,35 @@ import lombok.Getter;
 import lombok.Setter;
 
 public class Acao {
-    @Getter @Setter private String tipo;
+
+    /**
+     * Tipos de ação
+     */
+    public enum Tipo {
+        DISPARO, DESCONEXAO;
+
+        /**
+         * Pegar nome da ação
+         * @return nome da ação em letras minusculas
+         */
+        public String getName() {
+            return this.name().toLowerCase();
+        }
+    }
+
+    @Getter @Setter private Tipo tipo;
     @Getter @Setter private Double tempo;
     @Getter @Setter private String nome;
 
-    public Acao(String tipo, Double tempo, String nome) {
+    /**
+     * Construtor padrão para ação
+     * @param tipo tipo da ação
+     * @param tempo tempo de execução
+     * @param nome nome do componente
+     */
+    public Acao(Tipo tipo, Double tempo, String nome) {
         this.tipo = tipo;
         this.tempo = tempo;
         this.nome = nome;
     }
 }
-//externalizar o calculo de area do foguete
-//
--- src/main/java/br/uniriotec/pm20181/modelo/plano/PlanoVoo.java
+++ src/main/java/br/uniriotec/pm20181/modelo/plano/PlanoVoo.java
@@ -1,8 +1,22 @@
 package br.uniriotec.pm20181.modelo.plano;
 
+import lombok.Getter;
+
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
+/**
+ * Classe que representa um plano de vôo
+ */
 public class PlanoVoo {
-    private List<Acao> listaAcoes = new ArrayList<>();
+    @Getter private List<Acao> listaAcoes = new ArrayList<>();
+
+    /**
+     * Construtor que recebe um array de ações
+     * @param acoes array de ações
+     */
+    public PlanoVoo(Acao... acoes) {
+        listaAcoes.addAll(Arrays.asList(acoes));
+    }
 }
