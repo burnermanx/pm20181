23f2a8af4b799a900262f4fa159b1cfe753bc5cc
Thiago Passos <thiago.passos@mobicare.com.br>
2018-06-21 13:11:11 -0300

    Comentando classes de simulador de voo

--- src/main/java/br/uniriotec/pm20181/controle/HistoricoSimulador.java
+++ src/main/java/br/uniriotec/pm20181/controle/HistoricoSimulador.java
@@ -3,6 +3,9 @@ package br.uniriotec.pm20181.controle;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * Classe que representa o histórico do simulador
+ */
 public class HistoricoSimulador {
     static final String KEY_ALTITUDE = "altitude";
     static final String KEY_ACELERACAO = "aceleracao";
@@ -17,10 +20,16 @@ public class HistoricoSimulador {
         return this.tempo;
     }
 
+    /**
+     * Pega um valor do histórico dado a chave
+     */
     public String pegaValor(String chave) {
         return valores.get(chave);
     }
 
+    /**
+     * Insere um valor no histórico
+     */
     public void insereValor(String chave, String valor) {
         valores.put(chave, valor);
     }
--- src/main/java/br/uniriotec/pm20181/controle/Simulador.java
+++ src/main/java/br/uniriotec/pm20181/controle/Simulador.java
@@ -1,6 +1,6 @@
 package br.uniriotec.pm20181.controle;
 
-        import br.uniriotec.pm20181.modelo.ambiente.Ambiente;
+import br.uniriotec.pm20181.modelo.ambiente.Ambiente;
         import br.uniriotec.pm20181.modelo.foguete.Foguete;
         import br.uniriotec.pm20181.modelo.plano.PlanoVoo;
         import lombok.Getter;
@@ -8,6 +8,9 @@ package br.uniriotec.pm20181.controle;
         import java.util.ArrayList;
         import java.util.List;
 
+/**
+ * Classe responsável pela simulação de voo
+ */
 public class Simulador {
     private List<HistoricoSimulador> listaHistorico = new ArrayList<>();
 
@@ -18,10 +21,16 @@ public class Simulador {
         this.ambiente = ambiente;
     }
 
+    /**
+     * Executa uma simulação de voo
+     */
     public void executa(Foguete foguete, PlanoVoo planoVoo, int tempoVoo) {
 
     }
 
+    /**
+     * Retorna um item do histórico do voo, dado o indice
+     */
     public HistoricoSimulador pegaHistoricoIndice(int indice) {
         return listaHistorico.get(indice);
     }
